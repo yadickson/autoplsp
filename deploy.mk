ff1e5aa3a94ecac729d170459e97ad07cd52098b
Yadickson Soto <yadickson.soto@basedos.cl>
2019-04-26 13:11:28 -0400

    Add travis support

--- deploy.mk
+++
@@ -1,4 +0,0 @@
-all:
-	curl -s https://raw.githubusercontent.com/yadickson/private-key.gpg/1.0.0/private-key.gpg | gpg --import
-	mvn versions:set -DnewVersion=${TRAVIS_TAG}
-	mvn clean deploy -Dmaven.test.skip=true -P release --settings deploy.xml
